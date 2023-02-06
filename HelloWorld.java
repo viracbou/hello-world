8246171fe5aad17df469db4f2777878e0e126897
Virac Bou <vbou0000@email.cpcc.edu>
2023-02-06 04:11:09 -0500

    Revert "Modified HelloWorld Program"
    This reverts commit beeae12878ae771841741f6eb5f42a788870deb6.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld{
    public static void main(String[] args) {
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to orignal version using GitHub Desktop.");
    }  
 }
